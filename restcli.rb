4dab0b97081ed631f1bf5800f587d08c15b7ea9f
quangson91 <quangson91@gmail.com>
2020-09-13 11:05:13 +0700

    Update distribute files

--- restcli.rb
+++ restcli.rb
@@ -3,7 +3,7 @@ class Restcli < Formula
     desc "A missing commandline application for execute IntelliJ HTTP Client files"
     homepage "https://restcli.github.io/"  
     url "https://raw.githubusercontent.com/restcli/homebrew-restcli/master/restcli.zip"
-    sha256 "af6a4b85a7a9e0b26ba941bba4024ce54b58f8177bf28fd8ba9587fbace739fd"
+    sha256 "31fc4c80bc57b355c44617007e842a141817e77ad0f4ee396de1dbf3090d5152"
     license "MIT"
     version "1.7.0"
 
