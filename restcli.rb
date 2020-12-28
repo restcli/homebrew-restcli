af44cafa3cd1b901c7370966a29f23572f08eafc
QuangSon <quangson91@gmail.com>
2020-12-28 14:58:00 +0700

    Update restcli.rb

--- restcli.rb
+++ restcli.rb
@@ -2,10 +2,10 @@ require "formula"
 class Restcli < Formula
     desc "A missing commandline application for execute IntelliJ HTTP Client files"
     homepage "https://restcli.github.io/"  
-    url "https://github.com/restcli/restcli/releases/download/v1.7.3/brew_restcli.zip"
-    sha256 "f3db8e984599cb3992726b84cfd2d6a60fd10ca0557df8d2131a93521a8f7888"
+    url "https://github.com/restcli/restcli/releases/download/v1.7.4/brew_restcli.zip"
+    sha256 "7e8d5d7c64c31b3d5b3768a69960d0c11b419bafb6f41c0ac2410ab85b56e1cb"
     license "MIT"
-    version "1.7.3"
+    version "1.7.4"
 
     def install
         inreplace "brew/restcli", "##PREFIX##", "#{prefix}"
